b401188b94c95f16211c6b12c342d3d31d512826
Hong Ng <hong.yang.ng@gmail.com>
2023-07-15 13:30:50 +1000

    Modify Content component to transfer the content of rendering the course info in the Part component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,8 +8,19 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+
+const Part = (props) => {
+  return (
     <>
-      <p>{props.part} {props.exercise}</p>
+      <p>{props.part} {props.exercises}</p>
     </>
   )
 }
@@ -34,9 +45,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
+      <Content part1={part1} exercises1={exercises1}
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
